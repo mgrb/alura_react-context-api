f7f13d3eeb4b82e207105f886aeb1c30dae3281d
Marlos Ribeiro <marlos.ribeiro@ufpe.br>
2024-07-17 19:27:42 +0000

    WIP: Context carrinho

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -1,9 +1,29 @@
-import React from "react";
-import Produto from "./Produto";
-import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
+import produtos from "@/mocks/produtos.json";
+import { useContext } from "react";
+import Produto from "./Produto";
+import { CarrinhoContext } from "@/context/CarrinhoContext";
 
-const Produtos = ({ adicionarProduto }) => {
+const Produtos = () => {
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+
+  function adicionarProduto(produto) {
+    const temOProduto = carrinho.some((p) => p.id === produto.id);
+
+    if (!temOProduto) {
+      produto.quantidade = 1;
+      setCarrinho((carrinhoAnterior) => [...carrinhoAnterior, produto]);
+    }
+  }
+  setCarrinho((carrinhoAnterior) =>
+    carrinhoAnterior.map((p) => {
+      if (p.id === produto.id) {
+        p.quantidade += 1;
+      }
+      return p;
+    })
+  );
+
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -3,5 +3,11 @@ import { createContext } from "react";
 export const CarrinhoContext = createContext();
 
 export const CarrinhoProvider = ({ children }) => {
-  return <CarrinhoContext.Provider>{children}</CarrinhoContext.Provider>;
+  const [carrinho, setCarrinho] = useState([]);
+
+  return (
+    <CarrinhoContext.Provider value={{ carrinho, setCarrinho }}>
+      {children}
+    </CarrinhoContext.Provider>
+  );
 };
